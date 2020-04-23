58e0cfcb08353640ee7a71686975767f8984ee00
Jultis <31956888+Jultis@users.noreply.github.com>
2020-04-23 11:31:34 +0200

    Produced plots

--- Oblig_2/wolff.py
+++ Oblig_2/wolff.py
@@ -1,18 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+
+
+sys.setrecursionlimit(1500)
 from tqdm import tqdm
 
-class Wolff(object):
+class Wolff:
+    '''
+    Class for solving the Ising model in 1D or 2D,
+    using the Wolff algorithm
+    '''
     def __init__(self, Lx, Ly, T, J, MCsteps):
-        self.Lx = Lx
-        self.Ly = Ly
-        self.N = Lx*Ly
-        self.T = T
-        self.prob = 1 - np.exp(-2*J/T)
-        self.steps = MCsteps
-        s = np.random.randint(0, 2, (self.Lx, self.Ly))
+        self.Lx = Lx                                        #Spin in x-direction
+        self.Ly = Ly                                        #Spin in y-direction
+        self.N = Lx*Ly                                      #Total number of spins
+        self.T = T                                          #Temperature
+        self.prob = 1 - np.exp(-2*J/T)                      #Probability to add bond
+        self.steps = MCsteps                                #Number of Monte Carlo steps
+        s = np.random.randint(0, 2, (self.Lx, self.Ly))     #Spin matrix
         self.s = np.where(s==0, -1, s)
-        self.c = np.ones((self.Lx, self.Ly), dtype=bool)
+        self.c = np.ones((self.Lx, self.Ly), dtype=bool)    #Cluster matrix
 
 
         if self.Ly or self.Lx == 1:
@@ -21,14 +29,19 @@ class Wolff(object):
         else:
             self.correlation = self.correlation2D
 
-        # Observables
 
     def add_to_cluster(self, i, j, spin):
+        '''
+        Function for flipping spin with probability p = 1 - e^(-2J/T)
+        '''
         if self.s[i][j] == spin:
             if np.random.rand(1) < self.prob:
                 self.Wolff_alg(i, j, spin)
 
     def Wolff_alg(self, i, j, spin):
+        '''
+        Function for
+        '''
         self.c[i][j] = True
         self.s[i][j] *= -1
 
@@ -72,34 +85,55 @@ class Wolff(object):
 
     def MC(self):
         self.cr = 0
-        self.Msum = 0
-        for k in tqdm(range(int(self.steps / 5))):
+        nsave = int(self.steps / 25)
+        c1 = 0
+        c2 = 1
+
+        for k in range(int(self.steps / 5)):
             self.c[:] = False
 
             i = int(np.random.rand(1)*self.Lx)
             j = int(np.random.rand(1)*self.Ly)
             self.Wolff_alg(i, j, self.s[i][j])
-        for k in tqdm(range(self.steps)):
+        M = np.mean(self.s)
+        self.M1 = M
+        self.M2 = M**2
+        self.M4 = M**4
+        self.correlation()
+        for k in range(self.steps):
             self.c[:] = False
 
             i = int(np.random.rand(1)*self.Lx)
             j = int(np.random.rand(1)*self.Ly)
             self.Wolff_alg(i, j, self.s[i][j])
             self.correlation()
-            M = np.sum(self.s)
-            self.Msum += M / self.N
-        self.cr /= self.steps
-        self.Msum /= self.steps
+            c1 += 1
+            #if c1 == nsave:
+            #    c1 = 0
+            #    c2 += 1
+            M = np.mean(self.s)
+            self.M1 += M
+            self.M2 += M**2
+            self.M4 += M**4
+
+
+        self.cr /= (self.steps +1)
+        self.M1 /= (self.steps +1)
+        self.M2 /= (self.steps +1)
+        self.M4 /= (self.steps +1)
 
 
 if __name__ == "__main__":
 
     figpath = "Plots/"
+    fontsize = 15
 
-    b = True
+    b = False
     c = False
     d = False
-    f = False
+    f = True
+    fb = False
+    fc = False
 
     if b:
         Lx = 16
@@ -107,7 +141,7 @@ if __name__ == "__main__":
         T1 = 0.5
         T2 = 1
         J = 1
-        MCsteps = 100000
+        MCsteps = 30000
 
         a1 = Wolff(Lx, Ly, T1, J, MCsteps)
         a1.MC()
@@ -119,31 +153,175 @@ if __name__ == "__main__":
         analcorr2 = a2.anal_corr(r)
 
         fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-        ax[0].set_title("T/J = 0.5")
         ax[0].plot(r, analcorr1, label="Analytic")
         ax[0].plot(r, a1.cr, label="MC model")
-        ax[0].set_xlabel(r"$r$")
-        ax[0].set_ylabel(r'$C(r)$')
-        ax[0].legend()
+        ax[0].set_xlabel(r"$r$", fontsize=fontsize)
+        ax[0].set_ylabel(r'$C(r)$', fontsize=fontsize)
+        ax[0].legend(fontsize=fontsize)
+        ax[0].tick_params('both', labelsize= 15)
 
-        ax[1].set_title("T/J = 1")
         ax[1].plot(r, analcorr2, label="Analytic")
         ax[1].plot(r, a2.cr, label="MC model")
-        ax[1].set_xlabel(r"$r$")
-        ax[1].set_ylabel(r'$C(r)$')
-        ax[1].legend()
-        plt.savefig("2b.pdf")
+        ax[1].set_xlabel(r"$r$", fontsize=fontsize)
+        ax[1].set_ylabel(r'$C(r)$', fontsize=fontsize)
+        ax[1].legend(fontsize=fontsize)
+        ax[1].tick_params('both', labelsize= 15)
+
+        plt.tight_layout()
+        #plt.savefig(figpath + "b.pdf")
         plt.show()
 
     if c:
+        save = False
+
         Lx = Ly = 16
         J = 1
-        MCsteps = 30000
-        T = np.linspace(0.5, 3, 20)
+        MCsteps = 10000
+        T = np.linspace(0.1, 5, 40)
         m = np.zeros(len(T))
-        for i, t in enumerate(T):
-            a = Wolff(Lx, Ly, t, J, MCsteps)
-            a.MC()
-            m[i] = np.abs(a.Msum)
+        if save:
+            for i, t in enumerate(tqdm(T)):
+                a = Wolff(Lx, Ly, t, J, MCsteps)
+                a.MC()
+                m[i] = a.M1
+            np.save("m1_C", m)
+        else:
+            m = np.load("m1_C.npy")
+        plt.scatter(T, m, s = 6)
         plt.plot(T, m)
+        plt.xlabel(r"$T/J$", fontsize=fontsize)
+        plt.ylabel(r"$\langle m\rangle$", fontsize=fontsize)
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig(figpath + "c.pdf")
+        plt.show()
+
+    if d:
+        save = False
+
+        Lx = Ly = 16
+        J = 1
+        MCsteps = 10000
+        T = np.linspace(0.1, 5, 40)
+        m = np.zeros(len(T))
+        if save:
+            for i, t in enumerate(tqdm(T)):
+                a = Wolff(Lx, Ly, t, J, MCsteps)
+                a.MC()
+                m[i] = a.M2
+            np.save("m2_D", m)
+        else:
+            m = np.load("m2_D.npy")
+        plt.scatter(T, m, s = 6)
+        plt.plot(T, m)
+        plt.xlabel(r"$T/J$", fontsize=fontsize)
+        plt.ylabel(r"$\langle m^2\rangle$", fontsize=fontsize)
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig(figpath + "d.pdf")
+        plt.show()
+
+    if f:
+        save = True
+
+        J = 1
+        MCsteps = 10000
+        L = [8, 16, 32]
+        T = np.linspace(2.0, 3.0, 30)
+        m2 = np.zeros((len(L), len(T)))
+        m4 = m2.copy()
+        if save:
+            for i, l in enumerate(L):
+                for j, t in enumerate(tqdm(T)):
+                    a = Wolff(l, l, t, J, MCsteps)
+                    a.MC()
+                    m2[i][j] = a.M2
+                    m4[i][j] = a.M4
+            np.save("m2_F", m2)
+            np.save("m4_F", m4)
+        else:
+            m2 = np.load("m2_F.npy")
+            m4 = np.load("m4_F.npy")
+
+        for i in range(3):
+            gamma = m4[i]/m2[i]**2
+            plt.scatter(T, gamma, s = 6)
+            plt.plot(T, gamma, label= "L = %d" % L[i])
+        plt.xlabel(r"$T/J$", fontsize=fontsize)
+        plt.ylabel(r"$\Gamma$", fontsize=fontsize)
+        plt.axvline(x = 2/(np.log(1 + np.sqrt(2))))
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        #plt.savefig(figpath + "f.pdf")
+        plt.show()
+
+    if fb:
+        save = False
+
+        J = 1
+        MCsteps = 10000
+        L = [8, 16, 32]
+        T = np.linspace(2.1, 2.3, 40)
+        m2 = np.zeros((len(L), len(T)))
+        m4 = m2.copy()
+        if save:
+            for i, l in enumerate(L):
+                for j, t in enumerate(tqdm(T)):
+                    a = Wolff(l, l, t, J, MCsteps)
+                    a.MC()
+                    m2[i][j] = a.M2
+                    m4[i][j] = a.M4
+            np.save("m2_Fb", m2)
+            np.save("m4_Fb", m4)
+        else:
+            m2 = np.load("m2_Fb.npy")
+            m4 = np.load("m4_Fb.npy")
+
+        for i in range(3):
+            gamma = m4[i]/m2[i]**2
+            plt.scatter(T, gamma, s = 6)
+            plt.plot(T, gamma, label= "L = %d" % L[i])
+        plt.xlabel(r"$T/J$", fontsize=fontsize)
+        plt.ylabel(r"$\Gamma$", fontsize=fontsize)
+        plt.axvline(x = 2/(np.log(1 + np.sqrt(2))))
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig(figpath + "fb.pdf")
+        plt.show()
+
+    if fc:
+        save = True
+
+        J = 1
+        MCsteps = 10000
+        L = [8, 16, 32]
+        T = np.linspace(2.2, 2.3, 50)
+        m2 = np.zeros((len(L), len(T)))
+        m4 = m2.copy()
+        if save:
+            for i, l in enumerate(L):
+                for j, t in enumerate(tqdm(T)):
+                    a = Wolff(l, l, t, J, MCsteps)
+                    a.MC()
+                    m2[i][j] = a.M2
+                    m4[i][j] = a.M4
+            np.save("m2_Fc", m2)
+            np.save("m4_Fc", m4)
+        else:
+            m2 = np.load("m2_Fc.npy")
+            m4 = np.load("m4_Fc.npy")
+
+        for i in range(3):
+            gamma = m4[i]/m2[i]**2
+            plt.scatter(T, gamma, s = 6)
+            plt.plot(T, gamma, label= "L = %d" % L[i])
+        plt.xlabel(r"$T/J$", fontsize=fontsize)
+        plt.ylabel(r"$\Gamma$", fontsize=fontsize)
+        plt.axvline(x = 2/(np.log(1 + np.sqrt(2))))
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        plt.savefig(figpath + "fc.pdf")
         plt.show()
